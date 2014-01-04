a99c0c43c7ede3146d4d5e6f89dadbaa3220dd3a
Ted Gould <ted@gould.cx>
2014-01-03 22:53:10 -0600

    Fleshing out main

--- xdg-autostart-find.c
+++ xdg-autostart-find.c
@@ -20,10 +20,46 @@
 
 #include <gio/gio.h>
 
+/* Look at a directory, ensure it exists and then start looking for
+   new autostart keys we can launch */
+static void
+check_dir (gpointer pdir, gpointer pkeyhash)
+{
+
+
+}
+
+/* Enumerates all the different autostart directories we can have */
+static GList *
+find_autostart_dirs (void)
+{
+
+	return NULL;
+}
+
 int
 main (int argc, gchar * argv[])
 {
+	/* Allocate resources */
+	GList * autostart_dirs = NULL;
+	GHashTable * autostart_keys = g_hash_table_new_full(g_str_hash, g_str_equal, g_free, NULL);
+	GDBusConnection * bus = g_bus_get_sync(G_BUS_TYPE_SESSION, NULL, NULL);
 
+	/* Make sure we got a bus */
+	g_return_val_if_fail(bus != NULL, -1);
+
+	/* Find all the directories we need */
+	autostart_dirs = find_autostart_dirs();
+
+	/* For each one look at the entries */
+	g_list_foreach(autostart_dirs, check_dir, autostart_keys);
+
+	/* Clean up */
+	g_hash_table_destroy(autostart_keys);
+	g_list_free_full(autostart_dirs, g_free);
+
+	g_dbus_connection_flush_sync(bus, NULL, NULL);
+	g_object_unref(bus);
 
 	return 0;
 }
