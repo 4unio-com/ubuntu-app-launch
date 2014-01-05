bd86e05521a1d24ae358b7b18d072839cd9aadc2
Ted Gould <ted@gould.cx>
2014-01-05 13:15:11 -0600

    Figure out how main should work here

--- xdg-autostart-parse.c
+++ xdg-autostart-parse.c
@@ -17,11 +17,40 @@
  *     Ted Gould <ted.gould@canonical.com>
  */
 
-#include <glib.h>
+#include <gio/gio.h>
+#include "helpers.h"
+
+static GKeyFile *
+find_keyfile (const gchar * task_name)
+{
+
+
+	return NULL;
+}
 
 int
 main (int argc, gchar * argv[])
 {
+	const gchar * task_name = g_getenv("XDG_AUTOSTART_NAME");
+	g_return_val_if_fail(task_name != NULL, -1);
+
+	GDBusConnection * bus = g_bus_get_sync(G_BUS_TYPE_SESSION, NULL, NULL);
+	g_return_val_if_fail(bus != NULL, -1);
+
+	GKeyFile * keyfile = find_keyfile(task_name);
+
+	if (keyfile == NULL) {
+		return -1;
+	}
+
+	gchar * exec = g_key_file_get_string(keyfile, "Desktop Entry", "Exec", NULL);
+	g_return_val_if_fail(exec != NULL, -1);
+
+	set_upstart_variable("APP_EXEC", exec);
+	g_free(exec);
+
+	g_dbus_connection_flush_sync(bus, NULL, NULL);
+	g_object_unref(bus);
 
 	return 0;
 }
