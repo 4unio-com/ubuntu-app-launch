6e6d648af6eb0f36a2e988080f0a743b027b979f
Ted Gould <ted@gould.cx>
2016-12-02 16:49:23 -0600

    Flesh the test out a bit

--- tests/fuzz/desktop-fuzz.cpp
+++ tests/fuzz/desktop-fuzz.cpp
@@ -1,8 +1,41 @@
 
+#include <gio/gio.h>
+#include <glib.h>
+#include <memory>
 #include <string>
 
-extern "C" int LLVMFuzzerTestOneInput (const uint8_t * data, size_t size)
-{
+#include "application-info-desktop.h"
+#include "registry.h"
 
-	return 0;
+static void clear_keyfile(GKeyFile* keyfile)
+{
+    if (keyfile != nullptr)
+    {
+        g_key_file_free(keyfile);
+    }
+}
+
+extern "C" int LLVMFuzzerTestOneInput(const uint8_t* data, size_t size)
+{
+    auto keyfile = std::shared_ptr<GKeyFile>(g_key_file_new(), clear_keyfile);
+
+    GError* error = nullptr;
+    g_key_file_load_from_data(keyfile.get(), reinterpret_cast<const gchar*>(data), size, G_KEY_FILE_NONE, &error);
+    if (error != nullptr)
+    {
+        g_error_free(error);
+        return 0;
+    }
+
+    try
+    {
+        auto registry = std::make_shared<ubuntu::app_launch::Registry>();
+        auto appinfo = std::make_shared<ubuntu::app_launch::app_info::Desktop>(
+            keyfile, "/", "/", ubuntu::app_launch::app_info::DesktopFlags::ALLOW_NO_DISPLAY, registry);
+    }
+    catch (...)
+    {
+    }
+
+    return 0;
 }
